fd087960667fcfd1348902869bb4a5a20d11fe45
Luis Salazar <l4sh@users.noreply.github.com>
2025-04-22 03:25:26 +0000

    WIP grid layout

---
+++ src/GridLayout.ts
@@ -0,0 +1,62 @@
+export class GridLayout {
+  columns: number;
+  rows: number;
+  screenWidth: number;
+  screenHeight: number;
+  cellWidth: number;
+  cellHeight: number;
+  coordType: string;
+
+  constructor(
+    columns: number,
+    rows: number,
+    screenWidth: number,
+    screenHeight: number,
+    coordType?: "start" | "center" | "end"
+  ) {
+    this.columns = columns;
+    this.rows = rows;
+    this.screenWidth = screenWidth;
+    this.screenHeight = screenHeight;
+    this.cellWidth = screenWidth / columns;
+    this.cellHeight = screenHeight / rows;
+    this.coordType = coordType || "center"; // By default return coordinates to the center of the cell;
+  }
+
+  resize(screenWidth: number, screenHeight: number) {
+    this.screenWidth = screenWidth;
+    this.screenHeight = screenHeight;
+    this.cellWidth = screenWidth / this.columns;
+    this.cellHeight = screenHeight / this.rows;
+  }
+
+  coordToCell(xCoord: number, yCoord: number): { x: number; y: number } {
+    return {
+      x: Math.floor(xCoord / this.cellWidth),
+      y: Math.floor(yCoord / this.cellHeight),
+    };
+  }
+
+  cellToCoord(xCell: number, yCell: number): { x: number; y: number } {
+    let x = xCell * this.cellWidth;
+    let y = yCell * this.cellHeight;
+
+    if (this.coordType === "center") {
+      x += this.cellWidth / 2;
+      y += this.cellHeight / 2;
+    } else if (this.coordType === "end") {
+      x += this.cellWidth;
+      y += this.cellHeight;
+    }
+
+    return { x: x, y: y };
+  }
+
+  column(xCell: number): number {
+    return this.cellToCoord(xCell, 0).x;
+  }
+
+  row(yCell: number): number {
+    return this.cellToCoord(0, yCell).y;
+  }
+}
